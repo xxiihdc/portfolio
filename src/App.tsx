d763550016b3ff65c16bbb30cade7079a964fe3f
ductvh1269 <SUN-ASTERISK\tran.van.hoai.duc@b123178-pc.sun-asterisk.com>
2024-04-26 15:33:23 +0700

    add Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css';
+import Card from './components/Card/Card';
 import Header from './components/Header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const badgesData = [
+    { text: 'New', color: 'bg-red' },
+    { text: 'Sale'},
+  ];
+
+
+
   return (
     <div className="App">
       <div className="container-fluid">
         <Header/>
+        <Card badges={badgesData} />
       </div>
     </div>
   );
